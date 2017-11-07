8fe5e5e99acbe0f6b36d8993e1c347f100315233
VERANIKA <veranika.isakova89@gmail.com>
2017-11-07 12:14:10 +0100

    One-to-Many: Controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,15 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
+  validate :end_after_start?
+  validates :starts_at, :ends_at, :presence => true
+
+  private
+
+  def end_after_start
+
+    if ends_at < starts_at
+      errors.add(:ends_at, "must be after the start date")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  
+
   root to: 'pages#home'
 
   devise_for :users
+  resources :users, only: [:show]
+  resources :events, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
