8dbcb531cf39d02284fcabe76f66928f353f4e88
VERANIKA <veranika.isakova89@gmail.com>
2017-11-07 17:34:12 +0100

    Add Room Views

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,10 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 500 }
-  validate :end_after_start?
+  validate :end_after_start
   validates :starts_at, :ends_at, :presence => true
+  after_initialize :default_values
 
-  private
 
   def end_after_start
 
@@ -14,4 +14,13 @@ class Event < ApplicationRecord
       errors.add(:ends_at, "must be after the start date")
     end
   end
+
+
+  def default_values
+    self.price ||= 0
+    self.includes_food ||= false
+    self.includes_drinks ||= false
+    self.active ||= true
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+arno = User.create!(
+  email: 'arno@codaisseur.com',
+  password: '123456'
+)
+
+Event.create!(
+  name: "Photo Tour",
+  description: "I want to make photos of the massive fields of tulips, windmills, canals, and unique small towns and villages",
+  location: "Amsterdam",
+  price: 10,
+  capacity: 10,
+  includes_food: false,
+  includes_drinks: false,
+  starts_at: Date.parse('12-12-2017'),
+  ends_at: Date.parse('13-12-2017'),
+  active: true,
+  user: arno)
