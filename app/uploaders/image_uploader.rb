84cc506ca7f049efa35a4c047f583be7afaf71b6
VERANIKA <veranika.isakova89@gmail.com>
2017-11-09 15:47:05 +0100

    Create Photo: Views

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,11 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+    version :thumbnail do
+      eager
+      resize_to_fit(300, 300)
+      cloudinary_transformation :quality => 80
+    end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
+  resources :photos, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
