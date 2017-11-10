ad32eee469285c30d984c9447fdfc4802be5e13c
VERANIKA <veranika.isakova89@gmail.com>
2017-11-10 13:22:21 +0100

    Through Association: Controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @registration = current_user.registrations.create(registration_params)
+
+    redirect_to @registration.event, notice: "Thank you for registration!"
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(:status, :price, :guests_count)
+  end
+end
