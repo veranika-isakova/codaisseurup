fd5bbdd719d646c7fb6c6dae9c18174b3ec34edf
VERANIKA <veranika.isakova89@gmail.com>
2017-11-12 21:05:05 +0100

    Fix Association: Views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,15 @@
 class RegistrationsController < ApplicationController
   before_action :authenticate_user!
   def create
-    @registration = current_user.registrations.create(registration_params.merge(event_id: params[:event_id]))
+    @registration = current_user.registrations.new(registration_params.merge(event_id: params[:event_id]))
     @registration.set_total_price
+    @registration.save
     redirect_to @registration.event, notice: "Thank you for registration!"
   end
 
   private
 
   def registration_params
-    params.require(:registration).permit(:status, :price, :guests_count, :event_id)
+    params.require(:registration).permit(:guests_count)
   end
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,8 +3,8 @@ class Registration < ApplicationRecord
   belongs_to :event
 
   def set_total_price
-    price_of_event = event.price
-    amount_of_guests = self.guests_count
+    price_of_event = event.price.to_i
+    amount_of_guests = self.guests_count.to_i
     self.price = price_of_event * amount_of_guests
   end
 
